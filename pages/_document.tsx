5b4da2ee8db99a5273519e3cb05040edfeb499ab
iqbalfachry19 <ifachry@rocketmail.com>
2022-06-18 12:43:07 +0700

    hi

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,9 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="apple-touch-icon" href="/logo.png" />
+          <meta name="theme-color" content="#fff" />
           <link rel="shortcut icon" href="/logo.png" />
           <link
             href="https://fonts.googleapis.com/css2?family=Carter+One&family=Inter:wght@300;400;500;600&display=swap"
